36b4d10ef16f08c5b387f5c44ca848e6e85876cd
Josh Barrington <josh.s.barrington@gmail.com>
2019-10-15 22:22:26 +0100

    Added list exercise feature

--- app/workout/tests/test_exercise_api.py
+++ app/workout/tests/test_exercise_api.py
@@ -65,3 +65,22 @@ class PrivateExerciseApiTest(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], exercise.name)
+
+    def test_create_exercise_successful(self):
+        """Test create new exercise"""
+        payload = {'name': 'Plank'}
+        self.client.post(EXERCISE_URL, payload)
+
+        exists = Exercise.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+
+        self.assertTrue(exists)
+
+    def test_create_exercise_invalid(self):
+        """Test creating invalid ingredient fails"""
+        payload = {'name': ''}
+        res = self.client.post(EXERCISE_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
